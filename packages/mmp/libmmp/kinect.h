0b2e962433f8369af36f3e8b27edfd482635ecd2
wangyun <wangyun@yy.com>
2019-04-03 18:34:01 +0800

    添加注释

--- packages/mmp/libmmp/kinect.h
+++ packages/mmp/libmmp/kinect.h
@@ -7,10 +7,13 @@
 
 struct KinectListItem
 {
-	std::wstring name;
-	std::wstring child;
-	int index;
+	std::wstring name;	// mmd中骨骼点的日文名称，unicode编码
+	std::wstring child; // Kinect子节点名称
+	int index;			// mmd中骨骼点的索引
 };
 
+// Kinect中19个骨骼点和mmd中的映射关系
 extern std::map<std::wstring, KinectListItem> kinectBoneList;
+
+// Kinect设备模拟数据
 extern std::vector<std::map<std::wstring, glm::vec3>> bodyInfoList;
