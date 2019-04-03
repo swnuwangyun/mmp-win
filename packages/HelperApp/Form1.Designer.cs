3a4b2bb0dca3e4f437b2256f72b17dacef0e8383
wangyun <wangyun@yy.com>
2019-04-03 18:18:03 +0800

    添加代码生成工具，可以从JS项目中导入kinect映射关系，以及测试数据

---
+++ packages/HelperApp/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace HelperApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(376, 410);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(750, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(402, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(315, 410);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(750, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 425);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ packages/HelperApp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelperApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lines = this.richTextBox1.Text.Split('\n');
+            int offset = 3;
+
+            string code = "std::vector<std::map<std::wstring, glm::vec3>> bodyInfoList = {\r\n";
+            string temp = "";
+
+            while (true)
+            {
+                string[] items;
+
+                string name = lines[offset++];
+                items = name.Split('\"');
+                name = items[1];
+
+                string x = lines[offset++];
+                items = x.Split('\"');
+                x = items[3];
+
+                string y = lines[offset++];
+                items = y.Split('\"');
+                y = items[3];
+
+                string z = lines[offset++];
+                items = z.Split('\"');
+                z = items[3];
+
+                temp += string.Format("     {{ L\"{0}\", glm::vec3({1}, {2}, {3}) }},\r\n", name, x, y, z);
+
+                if (name=="SpineBase")
+                {
+                    // Begin
+                }
+                if (name=="ThumbRight")
+                {
+                    // End
+                    code += "   {\r\n";
+                    temp = temp.Substring(0, temp.Length - 3) + "\r\n";
+                    code += temp;
+                    code += "   },\r\n";
+                    temp = "";
+
+                    offset += 4;
+                }
+                offset++;
+
+                if (offset > lines.Length)
+                {
+                    code = code.Substring(0, code.Length - 3) + "\r\n";
+                    code += "};\r\n";
+                    break;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] lines = this.richTextBox2.Text.Split('\n');
+            int offset = 1;
+
+            string code = "std::map<std::wstring, KinectListItem> kinectBoneList = {\r\n";
+            string temp = "";
+
+            while (true)
+            {
+                string[] items;
+
+                string kinect = lines[offset++];
+                items = kinect.Split(':');
+                kinect = items[0].Trim();
+
+                string name = lines[offset++];
+                items = name.Split('\"');
+                name = items[1];
+
+                string child = lines[offset++];
+                items = child.Split('\"');
+                if (items.Length > 2)
+                    child = items[1];
+                else
+                {
+                    child = "";
+                    offset--;
+                }
+
+                temp += string.Format(" {{ L\"{0}\", {{ L\"{1}\", L\"{2}\", {3} }} }},\r\n", kinect, name, child, -1);
+                if (child == "")
+                    temp += "\r\n";
+
+                if (kinect == "SpineMid")
+                {
+                    // Begin
+                }
+                if (kinect == "FootRight")
+                {
+                    // End
+                    temp = temp.Substring(0, temp.Length - 2);
+                    temp = temp.Substring(0, temp.Length - 3) + "\r\n";
+                    code += temp;
+                    code += "};\r\n";
+                    break;
+                }
+                offset++;
+            }
+        }
+    }
+}
